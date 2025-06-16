63b54ce41d4f901ec622bd0bb490e2d7cdc86517
Pradeep M <159505538+PRADEEP131204@users.noreply.github.com>
2025-06-16 15:21:22 +0530

    Create Infix_to_Prefix.java

---
+++ STACK/Infix_to_Prefix.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Infix_to_Prefix {
+    
+    public static int precedence(char ch){
+      if(ch=='+' || ch=='-')return 1;
+      else if(ch=='*' || ch=='/')return 2;
+      else if(ch=='^')return 3;
+      return 0;
+    }
+    
+  
+    public static String infixtoPrefix(String s){
+      
+      StringBuilder res=new StringBuilder();
+      Stack<Character> ops=new Stack<>();
+      
+      for(int i=s.length()-1;i>=0;i--){
+        char ch=s.charAt(i);
+        
+        if(Character.isLetter(ch)){
+          res.append(ch);
+        }
+        
+        else if(ch==')'){
+          ops.push(')');
+        }
+        
+        else if(ch=='('){
+          while(ops.peek()!=')'){
+            res.append(ops.pop());
+          }
+          ops.pop(); //To remove '('
+        }
+        
+        else if(ch=='+' || ch=='-' || ch=='*' || ch=='/' || ch=='^' ){
+          while(!ops.isEmpty() && ops.peek()!=')' && 
+          ((precedence(ch)<precedence(ops.peek())) || (precedence(ch)==precedence(ops.peek()) && ch!='^')))
+        {
+            res.append(ops.pop());
+        }
+        ops.push(ch);
+      }
+      }
+        while(!ops.isEmpty()){
+            res.append(ops.pop());
+        }
+
+        return res.reverse().toString();
+    }
+    
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter Infix Expression: ");
+      String s=sc.nextLine();
+      System.out.println(infixtoPrefix(s));
+  }
+}
